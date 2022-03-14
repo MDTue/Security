528d08aadf63f5cc1007adabe9514af4516a7b2d
Margret <md601@arcor.de>
2022-03-14 17:25:53 +0100

    mit Rechten und UserMongoDB

--- src/main/java/com/example/demosecurity/SecurityConfig.java
+++ src/main/java/com/example/demosecurity/SecurityConfig.java
@@ -1,34 +1,33 @@
 package com.example.demosecurity;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("md")
-                .password("123").roles("ADMIN")
-                .and()
-                .withUser("Oskar")
-                .password("123").roles("USER");
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers("/api/greet").hasAnyRole("USER", "ADMIN")
-                .and()
+        http.csrf().disable()
                 .authorizeRequests()
+                .mvcMatchers("/api/greet").hasAnyRole("USER", "ADMIN")
                 .mvcMatchers("/api/admingreet").hasRole("ADMIN")
-                .and().authorizeRequests()
+                .mvcMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .antMatchers("/**").authenticated()
                 .and().formLogin()
                 .and().httpBasic();
---
+++ src/main/java/com/example/demosecurity/UserDocument.java
@@ -0,0 +1,14 @@
+package com.example.demosecurity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
+@Data
+public class UserDocument {
+    @Id
+    String email;
+    String passwort;
+    String role;
+}
